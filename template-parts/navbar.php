e4591dd6ed8f895e4c2b1912c57ac425312c7f26
semmson <drums.vienna@gmail.com>
2025-06-13 00:43:15 +0200

    f error

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -13,9 +13,9 @@
             <li><a href="elements.html">Elements Reference</a></li>
         </ul> -->
         <ul class="icons">
-            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-            <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-            <li><a href="#" class ="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-            <li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+            <li><a href="https://www.youtube.com/watch?v=0a7zjnjoraA&ab_channel=EliasHuemer" class="icon brands alt fa-youtube"><span class="label">Youtube</span></a></li>
+            <!-- <li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li> -->
+            <li><a href="https://www.instagram.com/eliashuemerproject/" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon brands alt fa-spotify"><span class="label">Spotify</span></a></li>
         </ul>
     </nav>
