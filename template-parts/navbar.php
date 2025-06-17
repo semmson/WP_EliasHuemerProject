ff5d2130ed7d64057c00205281b5a0062106e898
semmson <drums.vienna@gmail.com>
2025-06-17 16:08:09 +0200

    merge conflict resolved

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -13,16 +13,9 @@
             <li><a href="elements.html">Elements Reference</a></li>
         </ul> -->
         <ul class="icons">
-<<<<<<< HEAD
-            <li><a href="https://www.youtube.com/watch?v=0a7zjnjoraA&ab_channel=EliasHuemer" class="icon brands alt fa-youtube"><span class="label">Youtube</span></a></li>
-            <!-- <li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li> -->
-            <li><a href="https://www.instagram.com/eliashuemerproject/" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
-            <li><a href="#" class="icon brands alt fa-spotify"><span class="label">Spotify</span></a></li>
-=======
             <li><a target="_blank" href="https://www.youtube.com/watch?v=0a7zjnjoraA&ab_channel=EliasHuemer" class="icon brands alt fa-youtube"><span class="label">Youtube</span></a></li>
             <!-- <li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li> -->
             <li><a target="_blank" href="https://www.instagram.com/eliashuemerproject/" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
             <li><a target="_blank" href="#" class="icon brands alt fa-spotify"><span class="label">Spotify</span></a></li>
->>>>>>> 1e13092be51b84046195061ec0f9eb763e1b04bd
         </ul>
     </nav>
